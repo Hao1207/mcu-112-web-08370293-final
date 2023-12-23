67c1f97a0dc8666ae9bf048ee4f048dea9736d44
Hao <ben2000125@yahoo.com.tw>
2023-12-23 23:08:05 +0800

    feat(home-page): 建立標題列元件結構

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
